fecd2155d35022f148c3b6b96730101a30f027e0
safi miazi <mohibullamiazi@gmail.com>
2025-03-09 17:15:03 +0600

    Add user routes for customer registration, login, and logout

--- src/app/modules/users/users.routes.ts
+++ src/app/modules/users/users.routes.ts
@@ -1,1 +1,13 @@
-// users.routes.ts - users module
+import express from "express";
+
+
+const router = express.Router();
+
+
+
+
+router.post("/customer-registration", );
+router.post("/customer-login")
+router.post("/logout")
+
+export const userRoutes = router;
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,43 +1,15 @@
 import { Router } from "express";
-import { adminRoutes } from "../modules/admin/admin.routes";
-import { homeBannerRoutes } from "../modules/home_banner/home_banner.routes";
-import { homeAboutRoutes } from "../modules/home_about/home_about.routes";
-import { ServicesRoutes } from "../modules/home_services/home_services.routes";
-import { SectionHeaderRoutes } from "../modules/section_header/section_header.routes";
-import { projectRoutes } from "../modules/project/project.routes";
-import { CaseStudyRoutes } from "../modules/case_study/case_study.routes";
+import { userRoutes } from "../modules/users/users.routes";
+
 
 const router = Router();
 
 const moduleRoutes = [
   {
-    path: "/admin",
-    route: adminRoutes,
+    path: "/users",
+    route: userRoutes,
   },
-  {
-    path: "/home_banner",
-    route: homeBannerRoutes,
-  },
-  {
-    path: "/home_about",
-    route: homeAboutRoutes,
-  },
-  {
-    path: "/services",
-    route: ServicesRoutes,
-  },
-  {
-    path: "/project",
-    route: projectRoutes,
-  },
-  {
-    path: "/case_study",
-    route: CaseStudyRoutes,
-  },
-  {
-    path: "/section_header",
-    route: SectionHeaderRoutes,
-  },
+  
 
 ];
 
